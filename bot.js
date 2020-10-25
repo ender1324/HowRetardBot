c34b655b190954a8373cc886a49526e4ca13d60a
ender1324 <ender1324@abv.bg>
2020-10-25 11:58:33 +0200

    initial commit

---
+++ bot.js
@@ -0,0 +1,14 @@
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf("PUT YO TOKEN HERE")
+bot.catch((err, ctx) => {
+    console.log(`Encountered an error for ${ctx.updateType}`, err)
+  })
+bot.start((ctx) => ctx.reply("I'm alive, use /help to see the many of my command"))
+bot.help((ctx) => ctx.reply('use "/howretard [Something]" to check how retard is the something'))
+bot.hears(/howretard (.+)/, ({ match, reply }) => {
+    const randomnumber = Math.floor(Math.random() * 100) + "%";
+   reply(match[1] + " is " + randomnumber + " retarded")
+});
+
+ bot.launch()
