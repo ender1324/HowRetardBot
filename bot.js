235ec984b94ac7b7a851310ea170b60567cdc538
ender1324 <ender1324@abv.bg>
2020-10-25 12:11:51 +0200

    bot: Introduce a simple error handler

--- bot.js
+++ bot.js
@@ -8,7 +8,11 @@ bot.start((ctx) => ctx.reply("I'm alive, use /help to see the many of my command
 bot.help((ctx) => ctx.reply('use "/howretard [Something]" to check how retard is the something'))
 bot.hears(/howretard (.+)/, ({ match, reply }) => {
     const randomnumber = Math.floor(Math.random() * 100) + "%";
-   reply(match[1] + " is " + randomnumber + " retarded")
+   reply(match[1] + " is " + randomnumber + " retarded").catch((err) => { 
+    console.log(err)
+    const trimerr = err.toString().trim()
+    reply(trimerr)
+ })
 });
 
  bot.launch()
